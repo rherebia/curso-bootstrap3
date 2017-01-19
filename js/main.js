73ad267411d5bd8cce3b4e17500cfb5b8f1f7057
Renato Herebia <rherebia@gmail.com>
2017-01-19 08:30:12 -0300

    Assistindo aula 26

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function(){
     // equivalente ao load de nossa página
+    $("#modal01").modal({
+        backdrop: false,
+        keyboard: false
+    });
+    
+    $("#modal01").modal("show");
+    
+    setTimeout(function() {
+       $("#modal01").modal("hide")
+    }, 3000);
 });
