94db79c34131d4946a97959dfe6776e08614ee87
Renato Herebia <rherebia@gmail.com>
2017-04-06 20:01:36 -0400

    Assistindo aula 41

--- projeto-pratico/js/main.js
+++ projeto-pratico/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('a[href*=#]:not([href=#])').not(".carousel-control").click(function() {
+  $('#menu-navegacao a').not(".carousel-control").click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
